b20b30c0863affe7434d17d84f812f58cd5fe0d6
Andrew B <65259490+AndrewBergstrom@users.noreply.github.com>
2021-01-20 19:56:25 -1000

    function checkDriverAge added

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-var age = prompt("What is your age?");
 
-if (Number(age) < 18) {
-	alert("Sorry, you are too yound to drive this car. Powering off");
-} else if (Number(age) > 18) {
-	alert("Powering On. Enjoy the ride!");
-} else if (Number(age) === 18) {
-	alert("Congratulations on your first year of driving. Enjoy the ride!");
-}
+
+function checkDriverAge(age) {
+  var age = prompt("What is your age?");
+  if (Number(age) < 18) {
+    alert("Sorry, you are too young to drive this car. Powering off");
+  } else if (Number(age) > 18) {
+    alert("Powering On. Enjoy the ride!");
+  } else if (Number(age) === 18) {
+    alert("Congratulations on your first year of driving. Enjoy the ride!");
+  }
+};
+
+checkDriverAge(age);
 
 //1. Make the above code have a function called checkDriverAge(). Whenever you call this function, you will get prompted for age. Use Function Declaration to create this function.
 // Notice the benefit in having checkDriverAge() instead of copying and pasting the function everytime?
