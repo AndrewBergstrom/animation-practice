b2c5ab64f676f7b3a747f715c9e5627284d9cd2f
Andrew B <65259490+AndrewBergstrom@users.noreply.github.com>
2021-01-22 11:39:33 -1000

    callback function explained

--- script.js
+++ script.js
@@ -25,6 +25,7 @@ function addListAfterKeypress(event) {
     }
 }
 
+// addListAfterClick is a reference to the function without running it. The function will run once the event is triggered. IE: button click, or input keypress(enter)
 button.addEventListener("click", addListAfterClick)
 
 input.addEventListener("keypress",  addListAfterKeypress)
