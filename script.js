71e4146b6db73390172b07a587e6e61a79f9cafa
Andrew B <65259490+AndrewBergstrom@users.noreply.github.com>
2021-01-21 17:08:12 -1000

    signIn prompt working

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var database = [
   }
 ];
 
-var newsfeed = [
+var newsFeed = [
   {
     username:'Jakub',
     timeline:'So tired from all that learning'
@@ -18,3 +18,13 @@ var newsfeed = [
 
 var userNamePrompt = prompt("What's your username?");
 var passwordPrompt = prompt("What's your password");
+
+function signIn(username, password) {
+  if (username === database[0].username && password === database[0].password) {
+        console.log(newsFeed);
+  } else {
+    alert ("Sorry, wrong username and password");
+  }
+};
+
+signIn(userNamePrompt, passwordPrompt);
