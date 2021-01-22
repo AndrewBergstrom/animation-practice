35c3dfc30017a40bef6be2c7054a4b39ce0a3678
Andrew B <65259490+AndrewBergstrom@users.noreply.github.com>
2021-01-21 16:42:17 -1000

    object & array practice

---
+++ script.js
@@ -0,0 +1,32 @@
+// 1. Create an object that has properties "username" and "password". Fill those values in with strings.
+
+var user = {
+
+  username:"Pickle",
+  password:"Rick"
+
+}
+// 2. Create an array which contains the object you have made above and name the array "database".
+var database = [
+  {
+    username:"Pickle",
+    password:"Rick"
+  }
+  
+  ]
+
+  var newsfeed = [
+    {
+      username:"Andrew",
+      password:"human",
+
+    },
+    {
+      username:"Lindsay",
+      password:"human2",
+    },
+    {
+      username:"Ruth",
+      password:"KittyCat"
+    }
+  ]
