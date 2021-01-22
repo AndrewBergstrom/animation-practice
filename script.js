49f19f93293ef745ca95a2e68a47764ed0e7db81
Andrew B <65259490+AndrewBergstrom@users.noreply.github.com>
2021-01-22 11:35:03 -1000

    refactored code working

--- script.js
+++ script.js
@@ -2,25 +2,29 @@ var button = document.getElementById("enter");
 var input = document.getElementById("userInput");
 var ul = document.querySelector("ul");
 
-function inputLength () {
+function inputLength() {
   return input.value.length;
 }
 
-function createListElement(){
+function createListElement() {
   var li = document.createElement("li");
   li.appendChild(document.createTextNode(input.value));
   ul.appendChild(li);
   input.value = "";
 }
 
-button.addEventListener("click", function(){
-  if(inputLength() > 0){
-   createListElement();
-  } 
-})
+function addListAfterClick() {
+  if (inputLength() > 0) {
+    createListElement();
+  }
+}
 
-input.addEventListener("keypress", function(event){
-  if(inputLength() > 0 && event.keyCode === 13){
-   createListElement();
-  } 
-})
+function addListAfterKeypress(event) {
+    if (inputLength() > 0 && event.keyCode === 13) {
+      createListElement();
+    }
+}
+
+button.addEventListener("click", addListAfterClick)
+
+input.addEventListener("keypress",  addListAfterKeypress)
