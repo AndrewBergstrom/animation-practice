946d2bfeb822cb8745ac89324dd329e0ef40678c
Andrew B <65259490+AndrewBergstrom@users.noreply.github.com>
2021-01-21 16:57:45 -1000

    texting html script link with script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+var database = [
+  {
+    username:'Andrew',
+    password:'password'
+  }
+];
+
+var newsfeed = [
+  {
+    username:'Jakub',
+    timeline:'So tired from all that learning'
+  },
+  {
+    username:'Sally',
+    timeline:'JavaScript is soooo coool!'
+  }
+];
+
+var userNamePrompt = prompt("What's your username?");
+var passwordPrompt = prompt("What's your password");
