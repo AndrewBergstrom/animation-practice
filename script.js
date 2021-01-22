ad5874c2f29bf854b02abb1f4f4397b33ff38a86
Andrew B <65259490+AndrewBergstrom@users.noreply.github.com>
2021-01-21 14:58:55 -1000

    accessing triple nested array complete

--- script.js
+++ script.js
@@ -13,3 +13,20 @@ array.push("Kiwi");
 
 // 4. Remove "Apples" from the array.
 array.shift();
+
+// 5. Sort the array in reverse order. (Not alphabetical, but reverse
+// the current Array i.e. ['a', 'c', 'b'] becomes ['b', 'c', 'a'])
+var array = ["Blueberries", "Kiwi", "Oranges"]
+array.reverse();
+
+["Oranges", "Kiwi", "Blueberries"]
+
+// using this array,
+// var array2 = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+// access "Oranges".
+
+// My solution
+console.log(array2[1][1]);
+
+// Actual answer
+array2[1][1][0];
